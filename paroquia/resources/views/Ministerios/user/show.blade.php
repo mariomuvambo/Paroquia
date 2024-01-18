a02a673ee2d9ed5959947fb6d88644107a881d55
Fernandocuamba <mariomuvambo1@gmail.com>
2024-01-18 17:40:41 +0200

    MinisterioController
    update vieew

--- paroquia/resources/views/Ministerios/user/show.blade.php
+++ paroquia/resources/views/Ministerios/user/show.blade.php
@@ -10,29 +10,30 @@
 
     <div class="container">
 
-
         <div class="row">
-        @foreach($Userministerio as $Userministerio)
+            @foreach($Userministerio as $Userministerio)
             <div class="col-sm-4 mb-3 mb-sm-0">
                 <div class="card" style="width: 18rem;">
-                    <ul class="list-group list-group-flush">
-                        <li class="list-group-item">{{ $Userministerio->selecioneMinisterio }}</li>
-                        <li class="list-group-item">{{ $Userministerio->nome }} {{ $Userministerio->apelido }}</li>
-                        <li class="list-group-item"> {{ $Userministerio->contacto }}</li>
-                    </ul>
-                    <div class="card-footer">
-                    <a href="{{ route('ministerio.edit', ['id' => $Userministerio->id]) }}" class="btn 
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $Userministerio->selecioneMinisterio }}</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">{{ $Userministerio->nome }} {{
+                            $Userministerio->apelido }}</h6>
+                        <p class="card-text">{{ $Userministerio->contacto }}</p>
+                        <div class="card-footer">
+                            <small class="text-muted">
+                                <a href="{{ route('ministerio.edit', ['id' => $Userministerio->id]) }}" class="btn 
                         btn-primary details-btn">
-                            Editar
-                        </a>
-                        <a href=" {{ route('adminministerios.show') }} " class="btn 
+                                    Editar
+                                </a>
+                                <a href=" {{ route('adminministerios.show') }} " class="btn 
                         btn-secondary details-btn"> Detalhes</a>
+                            </small>
+                        </div>
+
                     </div>
                 </div>
             </div>
             @endforeach
-
-           
         </div>
 
     </div>
