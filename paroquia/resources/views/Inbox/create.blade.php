955c470d3fdaf10970073691b313a60cf850840d
Mario Muvambo <mariomuvambo1@gmail.com>
2024-02-02 16:32:01 +0200

    Update create.blade.php

--- paroquia/resources/views/Inbox/create.blade.php
+++ paroquia/resources/views/Inbox/create.blade.php
@@ -1,1 +1,16 @@
-<h1>notificacao</h1>
+<x-app-layout>
+
+    <x-slot name="header"> </x-slot>
+    @section('title', 'Create notificação')
+    @section('content')
+    <div>
+        <h2 id="subtitulo">Create Notificação</h2>
+    </div>
+    <hr style="margin-bottom: 15px;">
+
+
+    
+
+@endsection
+
+</x-app-layout>
